b6dab8d8d7534ca996f76e87e12d1909bebc101c
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-21 12:35:41 +0530

    passed all the server.js code now be making real code

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express"
 import cors from "cors"
-import 'dotenv/cofig'
+import 'dotenv/config'
 import http from "http"
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express()
+
+
+app.use(cors())
+app.use(express.json())
 
 
 
@@ -14,3 +18,15 @@ app.get("/",(req,res)=>{
 })
 
 const server = http.createServer(app)
+
+server.on('error', (error)=>{
+    if(error.code === "EAASSRINUSE") {
+        console.log(`Port ${PORT} is already in use`)
+        process.exit(1)
+    }
+    throw error
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server started on http://localhost:${PORT}`)
+})
