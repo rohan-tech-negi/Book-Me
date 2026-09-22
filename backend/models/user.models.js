310a097b4e3b159be82054f6e858edfd3dad7f0e
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-22 08:42:14 +0530

    passing multiple sections in the user schema

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,1 +1,31 @@
 import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    slug:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    businessName:{
+        
+    }
+})
