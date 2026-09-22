6db4777b4e3c78201c116db557e092258c4d111f
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-22 09:01:51 +0530

    done with the slugyfy

---
+++ backend/utils/slug.js
@@ -0,0 +1,10 @@
+const slugify = (value) =>{
+    return value 
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\s-]/g,'')
+    .replace(/\s+/g,'-')
+    .replace(/-+/g,'-')
+}
+
+export default slugify
