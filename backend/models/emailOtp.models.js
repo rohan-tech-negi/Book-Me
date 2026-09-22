563114b649069619f4e71196cc68a49f1b466db6
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-22 08:54:01 +0530

    passing differetn section in the emailotp

--- backend/models/emailOtp.models.js
+++ backend/models/emailOtp.models.js
@@ -1,1 +1,25 @@
 import mongoose from "mongoose"
+
+const emailOtpSchema = new mongoose.Schema(
+    {
+        email:{
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            index: true
+        },
+        purpose:{
+            type: String,
+            enum: ['registration', 'booking']
+        },
+        codeHash:{
+            type: String,
+            required: true
+        },
+        attempts:{
+            type: Number,
+            default: 0
+        }
+    }
+)
