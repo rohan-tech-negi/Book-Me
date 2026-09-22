86565c0854367e331f9e02a56b2a4e66441b52c2
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-22 08:34:56 +0530

    started the work

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,5 @@
 import mongoose from "mongoose";
+
+export const connectDB = async()=>{
+    await mongoose.connect("mongodb+srv://rohannegipixelpotion_db_user:2uzQc4VkhkCvV41J@cluster0.tahosjg.mongodb.net/")
+}
