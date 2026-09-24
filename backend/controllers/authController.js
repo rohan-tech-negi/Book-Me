46dde8598121edb834de393d21f5e5909c920de6
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-24 20:18:04 +0530

    passing the code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -186,5 +186,13 @@ export const loginUser = async(req,res)=>{
 }
 
 export const getMe = async(req,res)=>{
-    
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+
+        if(!user){
+            return res.status(404).json({message: "User not found"})
+        }
+    } catch (error) {
+        
+    }
 }
