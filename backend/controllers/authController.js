e472dedecc0544eafe7e0c6033fb4daca0f273c4
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-24 17:38:19 +0530

    now be making the actual code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,3 +3,7 @@ import jwt from "jsonwebtoken"
 import User from "../models/user.models.js"
 import { requestEmailOtp, verifyEmailOtp } from "../utils/emailOtp.js"
 import slugify from "../utils/slug.js"
+
+const createToken = (userId) =>{
+    return jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '7d'})
+}
