268c39a391e6435bce8852c97a821ac2356845b1
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-24 17:39:18 +0530

    make the touserresponse

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,3 +7,20 @@ import slugify from "../utils/slug.js"
 const createToken = (userId) =>{
     return jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '7d'})
 }
+
+
+const toUserResponse = (user) => ({
+  id: user._id,
+  name: user.name,
+  email: user.email,
+  slug: user.slug,
+  businessName: user.businessName,
+  businessDescription: user.businessDescription,
+  brandTheme: user.brandTheme,
+  brandAccent: user.brandAccent,
+  timezone: user.timezone,
+  googleCalendarConnected: user.googleCalendarConnected,
+  googleCalendarId: user.googleCalendarId,
+  payoutDetails: user.payoutDetails,
+  stripeConfigured: Boolean(process.env.STRIPE_SECRET_KEY),
+});
