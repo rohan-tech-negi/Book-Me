e9bb97ba248773cefba8e9c792de44380fde3668
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-24 17:36:53 +0530

    working on the controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,5 @@
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import User from "../models/user.models.js"
+import { requestEmailOtp, verifyEmailOtp } from "../utils/emailOtp.js"
+import slugify from "../utils/slug.js"
