61028aa355d516f3faedeaa58566fe290c039192
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-25 08:40:27 +0530

    passed the cde of the service model

---
+++ backend/models/service.models.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const serviceSchema = mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+            index: true
+        },
+        name:{
+            type: String,
+            required: true,
+            trim: true
+        }, 
+        duration:{
+            type:Number,
+            required: true,
+            min: 5,
+        },
+        price:{
+            type: Number ,
+            default: 0,
+            min: 0
+        },
+        description:{
+            type: String,
+            default: '',
+            trim: true
+        },
+        item:{
+            type: String,
+            default: 'C1.png'
+        },
+        isActive:{
+            type: Boolean,
+            default: false,
+            index: true
+        }
+    },{timestamps: true}
+)
