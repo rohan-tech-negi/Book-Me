44916bc932d3025f15da55494986cb64ce6c3320
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-25 08:23:36 +0530

    taking the use of the auth js in the code

---
+++ backend/middleware/auth.js
@@ -0,0 +1,22 @@
+import  Jwt  from "jsonwebtoken";
+
+const auth = (req,res,next)=>{
+    const authHeader  = req.header.authorization;
+
+    if(!authHeader || !authHeader.this.startWith('Bearer')){
+        return res.status(401).json({mesage: "No token provided"})
+    }
+
+    const token = authHeader.split(" ")(1)
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user  = {id: decode.userId}
+        next()
+    } catch (error) {
+        return res.status(401).json({message: "Invalid token"})
+    }
+}
+
+
+export default auth
