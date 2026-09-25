7ebd9d3e8aadbc6abd4f8bf695c1a90a8065f767
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-25 08:55:34 +0530

    making the service routes

---
+++ backend/routes/service.routes.js
@@ -0,0 +1,7 @@
+import express from "express"
+
+import { createServices, updateService, deleteService, listServices } from "../controllers/serviceCOntroller"
+
+
+import auth from '../middleware/auth.js'
+
