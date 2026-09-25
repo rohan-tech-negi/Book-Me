ebbc75ed3bd65c0238a7597a11b78787a72eca8f
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-25 09:06:29 +0530

    cretaed the avilability scehem now making it in use

--- backend/models/availability.models.js
+++ backend/models/availability.models.js
@@ -13,4 +13,28 @@ const slotSchema = new mongoose.Schema(
     },{_id: false}
 )
 
-const availability = 
+const availabilitySchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    },
+    dayOfWeek:{
+        type: Number,
+        required: true,
+        min: 0,
+        max: 6
+    },
+    slot:{
+        type: [slotSchema],
+        default: []
+    }
+},{timestamps: true})
+
+
+availability.index({userId: 1, dayOfWeek: 1}, {unique: true})
+
+const Availability = mongoose.model('Availability', availabilitySchema)
+
+export default Availability
