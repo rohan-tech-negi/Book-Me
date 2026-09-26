9554c4fd0e98e7db82b215b33012a29e7889342f
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-26 23:33:36 +0530

    making multiple controller like first listavailability

--- backend/controllers/availabilityController.js
+++ backend/controllers/availabilityController.js
@@ -1,2 +1,13 @@
 import Availability from "../models/availability.models";
 import { isValidTimeRande } from "../utils/time";
+
+export const listAvailability = async(req,res)=>{
+    try {
+        const availability = (await Availability.find({
+            userId: req.user.id
+        })).toSorted({dayOfWeek: 1})
+        res.json({availability})
+    } catch (error) {
+        res.status(500).json({message: "servicer error", error: error.message})
+    }
+}
