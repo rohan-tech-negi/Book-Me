ebcce369182309d9802b3f159dcdfd51275a77d9
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-26 23:06:48 +0530

    done with ther overlap.js

--- backend/utils/overlap.js
+++ backend/utils/overlap.js
@@ -1,1 +1,6 @@
 import {timeToMinutes}  from "./time.js"
+
+export const timeOverlap = (firstStart , firstEnd, secondStart, secondEnd) => {
+    return timeToMinutes(firstStart) < timeToMinutes(secondEnd)
+    && timeToMinutes(firstEnd > timeToMinutes(secondStart))
+}
